2f0e2c68bab685bb6ca219581491127945c0473d
Qing_ning <1750359613@qq.com>
2020-05-20 22:47:43 +0800

    2020/5/20

--- src/main/java/com.kuaishou.kcode/KcodeMain.java
+++ src/main/java/com.kuaishou.kcode/KcodeMain.java
@@ -1,7 +1,7 @@
 package com.kuaishou.kcode;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author kcode
@@ -11,16 +11,39 @@ public class KcodeMain {
 
     public static void main(String[] args) throws Exception {
         // "demo.data" 是你从网盘上下载的测试数据，这里直接填你的本地绝对路径
-        InputStream fileInputStream = new FileInputStream("demo.data");
+        InputStream fileInputStream = new FileInputStream("I:\\data\\warmup-test.data");
         KcodeQuestion question = new KcodeQuestion();
         // 准备数据
         question.prepare(fileInputStream);
         // 验证正确性
-        String result = question.getResult(1L, "");
-        if ("result".equals(result)) {
-            System.out.println("success");
-        } else {
-            System.out.println("fail");
+        BufferedReader br = null;
+        try {
+            int success=0,fail=0;
+            InputStream fis = new FileInputStream("I:\\data\\result-test.data");
+            br = new BufferedReader(new InputStreamReader(fis));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] s = line.split("\\|");
+                String[] s1 = s[0].split(",");
+                String result = question.getResult(Long.parseLong(s1[0]), s1[1]);
+                if (s[1].equals(result)) {
+                    success++;
+                    //System.out.println("success");
+                } else {
+                    fail++;
+                    System.out.println("fail:  "+s[1]+"  "+result);
+                    question.getResult(Long.parseLong(s1[0]), s1[1]);
+                }
+            }
+            System.out.println("正确/失败: "+success+"/"+fail);
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/com.kuaishou.kcode/KcodeQuestion.java
+++ src/main/java/com.kuaishou.kcode/KcodeQuestion.java
@@ -1,6 +1,7 @@
 package com.kuaishou.kcode;
 
-import java.io.InputStream;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author kcode
@@ -8,12 +9,58 @@ import java.io.InputStream;
  */
 public class KcodeQuestion {
 
+    private Map<String, Map<Integer, List<Integer>>> map = new HashMap();
+
+    private static long REDUCEVALUE = 1589700000000L;
+    private static long num = 0;
+
     /**
      * prepare() 方法用来接受输入数据集，数据集格式参考README.md
      *
      * @param inputStream
      */
     public void prepare(InputStream inputStream) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String line = null;
+        try {
+            Date start = new Date();
+            while ((line = br.readLine()) != null) {
+                String[] a = line.split(",");
+                String key = a[1];
+                long time = Long.parseLong(a[0]);
+                Map<Integer, List<Integer>> list = map.get(key);
+                if(list==null){
+                    list = new HashMap();
+                    List z = new ArrayList();
+                    z.add(Integer.parseInt(a[2]));
+                    list.put((int)(time/1000), z);
+                    map.put(key, list);
+                } else {
+                    List z = list.get((int)(time/1000));
+                    if(z == null){
+                        z = new ArrayList();
+                        z.add(Integer.parseInt(a[2]));
+                        list.put((int)(time/1000), z);
+                    } else {
+                        z.add(Integer.parseInt(a[2]));
+                    }
+                }
+                if(++num%1000000==0) {
+                    System.out.println("成功读取" + num + "条数据，总耗时" + (new Date().getTime() - start.getTime()) + "ms");
+                }
+            }
+            System.out.println("读取完毕，总共"+num+"条数据(我太难了)，总耗时"+(new Date().getTime()-start.getTime())+"ms");
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     /**
@@ -24,7 +71,31 @@ public class KcodeQuestion {
      * @param methodName 方法名称
      */
     public String getResult(Long timestamp, String methodName) {
-        // do something
-        return "QPS,P99,P50,AVG,MAX";
+        int QPS,P99,P50,AVG,MAX;
+        List<Integer> cz = map.get(methodName).get(timestamp.intValue());
+        Collections.sort(cz);
+        QPS = cz.size();
+
+        P99 = getPoint(cz,0.99);
+        P50 = getPoint(cz, 0.5);
+
+
+        int sum = 0;
+        for(int z:cz)
+            sum+=z;
+        AVG = (int)Math.ceil(1.0*sum/QPS);
+        MAX = cz.get(QPS-1);
+
+        return QPS+","+P99+","+P50+","+AVG+","+MAX;
+    }
+
+    private int getPoint(List<Integer> cz,double p){
+        double i = cz.size()*p;
+        if(i-(int)i == 0){
+            return cz.get((int)i-1);
+            //return (int)Math.ceil((double)(cz.get((int)i)+cz.get((int)i-1))/2);
+        } else {
+            return cz.get((int)Math.ceil(i-1));
+        }
     }
 }
